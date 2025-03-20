003b3dada13844466554b3aa656db3cc08cad695
jonejdazaimi <JonZai19@htl-shkoder.com>
2025-03-20 08:03:24 +0100

    neue klasse quardat

--- src/Quadrat.java
+++ src/Quadrat.java
@@ -1,4 +1,12 @@
-public class Quadrat {
+public class Quadrat extends Pyramide{
 
+    private double hoehe;
+    public Quadrat(double hoehe) {
+        super(hoehe);
+    }
 
+    @Override
+    public double calcVolumen() {
+        return hoehe*hoehe;
+    }
 }
